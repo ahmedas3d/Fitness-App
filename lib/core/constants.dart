0a19e960f3996b576ae2add767a8057f3c8fa9c9
deadcodee1 <anaahmedasaad.4@gmail.com>
2025-02-21 01:59:25 +0200

    first commit

---
+++ lib/core/constants.dart
@@ -0,0 +1,42 @@
+// constants.dart
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color fontColor1 = Color(0xFFFFFFFF);
+  static const Color fontColor2 = Color(0xFFD3D3D3);
+  static const Color bgColor = Color(0xFF242424);
+  static const Color secondColor90 = Color(0xFF3A3A3A);
+  static const Color secondColor80 = Color(0xFF505050);
+  static const Color secondColor70 = Color(0xFF666666);
+  static const Color secondColor60 = Color(0xFF7C7C7C);
+  static const Color secondColor50 = Color(0xFF919191);
+  static const Color secondColor40 = Color(0xFFA7A7A7);
+  static const Color secondColor30 = Color(0xFFBDBDBD);
+  static const Color secondColor20 = Color(0xFFE9E9E9);
+  static const Color mainColor = Color(0xFFFF4100);
+  static const Color mainColor10 = Color(0xFFFFA080);
+  static const Color mainColor20 = Color(0xFFFF8D66);
+  static const Color mainColor30 = Color(0xFFFF7A4D);
+  static const Color mainColor40 = Color(0xFFFF6733);
+  static const Color mainColor50 = Color(0xFFFF541A);
+  static const Color mainColor60 = Color(0xFFE52800);
+  static const Color mainColor70 = Color(0xFFCC0E00);
+  static const Color mainColor80 = Color(0xFFB20000);
+  static const Color mainColor90 = Color(0xFF990000);
+}
+
+class AppTextStyles {
+  static const TextStyle heading1 = TextStyle(fontSize: 31);
+  static const TextStyle heading2 = TextStyle(fontSize: 24);
+  static const TextStyle heading3 = TextStyle(fontSize: 20);
+  static const TextStyle bodyText1 = TextStyle(fontSize: 16);
+  static const TextStyle bodyText2 = TextStyle(fontSize: 13);
+  static const TextStyle bodyText3 = TextStyle(fontSize: 10);
+}
+
+class AppImages {
+  static const String logo = 'assets/icons/logo.svg';
+  static const String background = 'assets/images/background.jpg';
+  static const String background2 = 'assets/images/background2.jpg';
+}
